4e5099aef439c2c86e0616eac4b9133f41f10c33
Leandro Liduvino <leandro.augusto460@gmail.com>
2021-07-24 13:01:25 -0300

    add header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import format from 'date-fns/format';
+import ptBR from 'date-fns/locale/pt-BR';
+
+import styles from './header.module.scss';
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', { locale: ptBR });
+
+  return (
+    <header className={styles.container}>
+      <Image width="100%" height="100%" src="/logo.svg" alt="Devcast logo" />
+      <p>O melhor para você ouvir. Sempre!</p>
+      <span>{currentDate}</span>
+    </header>
+  );
+}
